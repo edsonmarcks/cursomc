172dea91b5f0efd80890ad5661a977404edda770
Edson Marques <edsonmarcks@hotmail.com>
2022-03-05 09:08:46 -0300

    Criado o banco de dados H2, criacao automatica das tabelas

--- src/main/java/com/edsonmarcks/curso/domain/Categoria.java
+++ src/main/java/com/edsonmarcks/curso/domain/Categoria.java
@@ -3,10 +3,21 @@ package com.edsonmarcks.curso.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length = 100)
 	private String nome;
 		
 	public Categoria() {
