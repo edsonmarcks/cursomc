c3c632ce5a28534d8d40d3e2ccf4cbe0db23b7cd
Edson Marques <edsonmarcks@hotmail.com>
2022-03-05 19:23:26 -0300

    Adicionado inserts na via jpa na tabela categoria

--- src/main/java/com/edsonmarcks/curso/CursoApplication.java
+++ src/main/java/com/edsonmarcks/curso/CursoApplication.java
@@ -1,13 +1,30 @@
 package com.edsonmarcks.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoApplication {
+import com.edsonmarcks.curso.domain.Categoria;
+import com.edsonmarcks.curso.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoApplication implements CommandLineRunner{
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null,"Escritório");
+		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
+		
 	}
 
 }
